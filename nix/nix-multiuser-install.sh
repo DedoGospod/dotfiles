3bc51763a3ba483c200c9fa77434fab6d0511256
DedoGospod <dylanlazarov2002@protonmail.com>
2025-11-17 15:28:48 +0800

    added font fix in nix-multiuser

--- nix/nix-multiuser-install.sh
+++ nix/nix-multiuser-install.sh
@@ -9,5 +9,16 @@ sudo systemctl daemon-reload
 sudo systemctl restart nix-daemon
 echo "✅ Nix daemon restarted."
 
+# Give nix access to system fonts
+sudo mkdir -p /etc/fonts
+sudo ln -sf /etc/fonts/fonts.conf /etc/fonts/fonts.conf
+
+# For user specific fonts
+sudo mkdir -p /usr/share/fonts
+sudo ln -sf /usr/share/fonts /usr/share/fonts
+
+# Clear font cache
+fc-cache -fv
+
 echo "Log out and log back in for changes to take effect"
 echo "vim into /etc/nix/nix.conf and on a new line add: experimental-features = nix-command flakes "
--- nix/nix-singleuser-install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
