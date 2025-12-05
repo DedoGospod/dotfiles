bc3a87f8227211c3d9a73c2785d52b5f7259ec60
DedoGospod <dylanlazarov2002@protonmail.com>
2025-12-05 21:34:51 +0800

    made functions more readable

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -3,10 +3,27 @@
 ### Custom Zsh Functions ###
 
 # Automatically do an ls after each zl command
-zl() { if [ -z "$@" ]; then ls; else z "$@" && ls; fi; } # Automatically do an ls after each zl command
+zl() {
+    if [ -z "$@" ]; then
+        ls -1 --color=always --group-directories-first
+    else
+        z "$@" && \
+        ls -1 --color=always --group-directories-first
+    fi;
+}
 
 # nv works for root and user files
 nv() {
-  if [ -z "$1" ]; then command nvim; return; fi
-  if [ -f "$1" ] && [ ! -O "$1" ]; then echo "Using sudoedit for root file: $1"; command sudoedit "$@"; else command nvim "$@"; fi
+    if [ -z "$1" ]; then
+        command nvim
+        return
+    fi
+    
+    if [ -f "$1" ] && [ ! -O "$1" ]; then
+        echo "Using sudoedit for root file: $1"
+        command sudoedit "$@"
+    
+    else
+        command nvim "$@"
+    fi
 }
