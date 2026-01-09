a7f6d4481c474c61edd93a1605635bfd77276c5e
DedoGospod <dylanlazarov2002@protonmail.com>
2026-01-10 00:15:51 +0800

    moved scripts to dotfiles

---
+++ scripts/user-scripts/.local/bin/gsettings.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.nautilus.preferences default-sort-order 'type'
+gsettings set org.gnome.desktop.privacy remember-recent-files false
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
