df7c2ab2fb6f881a80b2c542d63e6f69f4688652
DedoGospod <dylanlazarov2002@protonmail.com>
2026-01-23 14:46:41 +0800

    loop through function directory and sources all files inside

--- scripts/services-systemd.sh
+++ scripts/services-systemd.sh
@@ -3,13 +3,18 @@
 # ==============================================================================
 #  Setup & Dependencies
 # ==============================================================================
-FUNCTIONS_DIR="$HOME/dotfiles/scripts/setup-scripts/functions/"
 
-if [[ -f "$FUNCTIONS_DIR/systemd-setup-functions.sh" ]]; then
-    # shellcheck source=/dev/null
-    source "$FUNCTIONS_DIR/systemd-setup-functions.sh"
+# Source functions
+FUNCTIONS_DIR="$HOME/dotfiles/scripts/setup-scripts/functions/"
+if [[ -d "$FUNCTIONS_DIR" ]]; then
+    for func_file in "$FUNCTIONS_DIR"/*; do
+        if [[ -f "$func_file" ]]; then
+            # shellcheck source=/dev/null
+            source "$func_file"
+        fi
+    done
 else
-    echo "ERROR: systemd-setup-functions.sh not found!"
+    echo "Directory $FUNCTIONS_DIR not found"
     exit 1
 fi
 
@@ -29,10 +34,10 @@ systemctl --user daemon-reload
 echo ""
 log_info "--- Configuring System Services ---"
 
-manage_service "NetworkManager.service"         ""      "enable"  "Network management"      "Y"
-manage_service "bluetooth.service"              ""      "enable"  "Bluetooth connectivity"  "n"
-manage_service "power-profiles-daemon.service"  ""      "enable"  "Power profiles"          "Y"
-manage_service "ufw.service"                    ""      "enable"  "Enable firewall"         "Y"
+manage_service "NetworkManager.service" "" "enable" "Network management" "Y"
+manage_service "bluetooth.service" "" "enable" "Bluetooth connectivity" "n"
+manage_service "power-profiles-daemon.service" "" "enable" "Power profiles" "Y"
+manage_service "ufw.service" "" "enable" "Enable firewall" "Y"
 
 # Timers
 
@@ -49,12 +54,12 @@ if [[ "${XDG_SESSION_DESKTOP:-}" == "Hyprland" || -n "${HYPRLAND_INSTANCE_SIGNAT
 fi
 
 if $IS_HYPRLAND; then
-    manage_service "hypridle.service"        "--user"  "enable"  "Idle daemon"             "Y"
-    manage_service "hyprpaper.service"       "--user"  "enable"  "Wallpaper daemon"        "Y"
-    manage_service "pyprland.service"        "--user"  "enable"  "Pyprland plugins"        "Y"
-    manage_service "hyprpolkitagent.service" "--user"  "enable"  "Polkit Authentication"   "Y"
-    manage_service "waybar.service"          "--user"  "enable"  "Status bar"              "Y"
-    manage_service "hyprsunset.service"      "--user"  "enable"  "blue light filter"       "Y"
+    manage_service "hypridle.service" "--user" "enable" "Idle daemon" "Y"
+    manage_service "hyprpaper.service" "--user" "enable" "Wallpaper daemon" "Y"
+    manage_service "pyprland.service" "--user" "enable" "Pyprland plugins" "Y"
+    manage_service "hyprpolkitagent.service" "--user" "enable" "Polkit Authentication" "Y"
+    manage_service "waybar.service" "--user" "enable" "Status bar" "Y"
+    manage_service "hyprsunset.service" "--user" "enable" "blue light filter" "Y"
 else
     echo "  [User] Not in Hyprland. Skipping Hyprland-specific services."
 fi
@@ -70,12 +75,12 @@ select_exclusive_service "Blue light filter" "--user" "wlsunset.service" "hyprsu
 # General User Services
 # ------------------------------------------------------------------------------
 
-manage_service "wayland-pipewire-idle-inhibit.service" "--user"  "enable"  "Prevent sleep when playing audio" "Y"
-manage_service "swaync.service"                        "--user"  "enable"  "Notification daemon"              "Y"
-manage_service "obs.service"                           "--user"  "enable"  "OBS-STUDIO"                       "n"
+manage_service "wayland-pipewire-idle-inhibit.service" "--user" "enable" "Prevent sleep when playing audio" "Y"
+manage_service "swaync.service" "--user" "enable" "Notification daemon" "Y"
+manage_service "obs.service" "--user" "enable" "OBS-STUDIO" "n"
 
 # Timers
-manage_service "gearlever-update.timer"                "--user"  "enable"  "Gearlever auto-update"            "Y"
+manage_service "gearlever-update.timer" "--user" "enable" "Gearlever auto-update" "Y"
 
 # ==============================================================================
 #  Completion
