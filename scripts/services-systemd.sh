4cc9ef6f7845caa9190a9280eb6ed40824fb7ac1
DedoGospod <dylanlazarov2002@protonmail.com>
2026-01-23 20:05:11 +0800

    readability

--- scripts/services-systemd.sh
+++ scripts/services-systemd.sh
@@ -34,10 +34,10 @@ systemctl --user daemon-reload
 echo ""
 log_info "--- Configuring System Services ---"
 
-manage_service "NetworkManager.service" "" "enable" "Network management" "Y"
-manage_service "bluetooth.service" "" "enable" "Bluetooth connectivity" "n"
-manage_service "power-profiles-daemon.service" "" "enable" "Power profiles" "Y"
-manage_service "ufw.service" "" "enable" "Enable firewall" "Y"
+manage_service "NetworkManager.service"         "" "enable" "Network management"     "Y"
+manage_service "bluetooth.service"              "" "enable" "Bluetooth connectivity" "n"
+manage_service "power-profiles-daemon.service"  "" "enable" "Power profiles"         "Y"
+manage_service "ufw.service"                    "" "enable" "Enable firewall"        "Y"
 
 # Timers
 
@@ -54,12 +54,12 @@ if [[ "${XDG_SESSION_DESKTOP:-}" == "Hyprland" || -n "${HYPRLAND_INSTANCE_SIGNAT
 fi
 
 if $IS_HYPRLAND; then
-    manage_service "hypridle.service" "--user" "enable" "Idle daemon" "Y"
-    manage_service "hyprpaper.service" "--user" "enable" "Wallpaper daemon" "Y"
-    manage_service "pyprland.service" "--user" "enable" "Pyprland plugins" "Y"
-    manage_service "hyprpolkitagent.service" "--user" "enable" "Polkit Authentication" "Y"
-    manage_service "waybar.service" "--user" "enable" "Status bar" "Y"
-    manage_service "hyprsunset.service" "--user" "enable" "blue light filter" "Y"
+    manage_service "hypridle.service"         "--user" "enable" "Idle daemon"           "Y"
+    manage_service "hyprpaper.service"        "--user" "enable" "Wallpaper daemon"      "Y"
+    manage_service "pyprland.service"         "--user" "enable" "Pyprland plugins"      "Y"
+    manage_service "hyprpolkitagent.service"  "--user" "enable" "Polkit Authentication" "Y"
+    manage_service "waybar.service"           "--user" "enable" "Status bar"            "Y"
+    manage_service "hyprsunset.service"       "--user" "enable" "blue light filter"     "Y"
 else
     echo "  [User] Not in Hyprland. Skipping Hyprland-specific services."
 fi
@@ -68,19 +68,19 @@ fi
 # Conditional Selection
 # ------------------------------------------------------------------------------
 
-select_exclusive_service "Blue light filter" "--user" "wlsunset.service" "hyprsunset.service"
-# select_exclusive_service "Status bar"        "--user" "waybar.service" "ironbar.service"
+select_exclusive_service "Blue light filter"            "--user" "wlsunset.service" "hyprsunset.service"
+# select_exclusive_service "Status bar"                   "--user" "waybar.service" "ironbar.service"
 
 # ------------------------------------------------------------------------------
 # General User Services
 # ------------------------------------------------------------------------------
 
-manage_service "wayland-pipewire-idle-inhibit.service" "--user" "enable" "Prevent sleep when playing audio" "Y"
-manage_service "swaync.service" "--user" "enable" "Notification daemon" "Y"
-manage_service "obs.service" "--user" "enable" "OBS-STUDIO" "n"
+manage_service "wayland-pipewire-idle-inhibit.service"  "--user" "enable" "Prevent sleep when playing audio"  "Y"
+manage_service "swaync.service"                         "--user" "enable" "Notification daemon"               "Y"
+manage_service "obs.service"                            "--user" "enable" "OBS-STUDIO"                        "n"
 
 # Timers
-manage_service "gearlever-update.timer" "--user" "enable" "Gearlever auto-update" "Y"
+manage_service "gearlever-update.timer"                 "--user" "enable" "Gearlever auto-update"             "Y"
 
 # ==============================================================================
 #  Completion
