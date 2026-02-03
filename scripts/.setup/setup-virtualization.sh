eca73855edeb2d8bd6b7bc49c29a65a248803a78
DedoGospod <dylanlazarov2002@protonmail.com>
2026-02-03 14:55:24 +0800

    setup virtualization

--- scripts/.setup/setup-virtualization.sh
+++ scripts/.setup/setup-virtualization.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-set -o pipefail
-
 # Colors for logging
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -11,14 +8,63 @@ BLUE='\033[0;34m'
 NC='\033[0m'
 
 # Helper Functions
-header() { echo -e "\n${BLUE}==== $1 ====${NC}"; }
-log() { echo -e "${GREEN}[INFO]${NC} $1"; }
-warn() { echo -e "${YELLOW}[WARN]${NC} $1"; }
-error() { echo -e "${RED}[ERROR]${NC} $1"; }
+header()  { echo -e "\n${BLUE}==== $1 ====${NC}"; }
+log()     { echo -e "${GREEN}[INFO]${NC} $1"; }
+warn()    { echo -e "${YELLOW}[WARN]${NC} $1"; }
+error()   { echo -e "${RED}[ERROR]${NC} $1"; }
 success() { echo -e "${GREEN}[SUCCESS]${NC} $1"; }
 
 log_task() { echo -ne "${GREEN}[INFO]${NC} $1... "; }
-ok() { echo -e "${GREEN}Done.${NC}"; }
-fail() { echo -e "${RED}Failed.${NC}"; }
+ok()       { echo -e "${GREEN}Done.${NC}"; }
+fail()     { echo -e "${RED}Failed.${NC}"; }
 
-header "Configuring Virtualization"
+# Install necessary packages if needed
+log_task "Installing necessary packages"
+if sudo pacman -S --needed --noconfirm qemu-full virt-manager dnsmasq swtpm; then
+    ok
+else
+    fail
+fi
+
+# Add user to libvirt group
+group="libvirt"
+if id -nG "$USER" | grep -qw "$group"; then
+    log_task "User already in $group"
+    ok
+else
+    log_task "Adding $USER to $group"
+    if sudo usermod -aG "$group" "$USER"; then
+        touch /tmp/reboot_required
+        ok
+    else
+        error "Failed to add user to group."
+        exit 1
+    fi
+fi
+
+# Enable services 
+services=("libvirtd.service" "libvirtd.socket")
+for svc in "${services[@]}"; do
+    if systemctl is-active --quiet "$svc"; then
+        log_task "$svc is already running"
+        ok
+    else
+        log_task "Starting $svc..."
+        sudo systemctl enable --now "$svc"
+        ok
+    fi
+done
+
+# Network start
+net="default"
+if ! sudo virsh net-list --all | grep -q "$net"; then
+    log_task "Creating default network from template..."
+    sudo virsh net-define /etc/libvirt/qemu/networks/default.xml
+    ok
+fi
+
+# Start default network if its down
+sudo virsh net-start "$net" 2>/dev/null
+
+# Set default network to autostart
+sudo virsh net-autostart "$net" 2>/dev/null
