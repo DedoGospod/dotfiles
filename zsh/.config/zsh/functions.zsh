8a6df3fb93c3425614c5761486da0c393068597e
DedoGospod <dylanlazarov2002@protonmail.com>
2026-02-14 16:43:24 +0800

    more robust syntax

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -4,7 +4,7 @@
 
 # zl: Navigate with 'z' and immediately list directory contents
 zl() {
-    if [ -z "$@" ]; then
+    if [[ -z "$@" ]]; then
         ls -1 --color=always --group-directories-first
     else
         z "$@" && \
@@ -14,7 +14,7 @@ zl() {
 
 # zlh: Navigate with 'z' and list ALL contents (including hidden)
 zlh() {
-    if [ -z "$@" ]; then
+    if [[ -z "$@" ]]; then
         ls -1A --color=always --group-directories-first
     else 
         z "$@" && \
