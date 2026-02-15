e379ea1c7963cd958da526e76e8ca28b3fb13fc5
DedoGospod <dylanlazarov2002@protonmail.com>
2026-02-15 14:09:49 +0800

    added global colors and tldr function to open via nvim

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -1,5 +1,17 @@
 #!/usr/bin/env zsh
 
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+
+warn()    { echo -e "${YELLOW}[WARN]${NC} $1"; }
+error()   { echo -e "${RED}[ERROR]${NC} $1"; }
+usage()   { echo -e "${CYAN}[Usage]${NC} $1"; } 
+success() { echo -e "${GREEN}[SUCCESS]${NC} $1"; }
+success() { echo -e "${GREEN}[SUCCESS]${NC} $1"; }
+
 # NAVIGATION -------------------------------------------------------------------
 
 # zl: Navigate with 'z' and immediately list directory contents
@@ -42,6 +54,16 @@ nv() {
     fi
 }
 
+tldr() {
+    if [[ -n "$1" ]]; then
+        command tldr "$@" | nvim -R -c "set ft=help" -
+    else
+        error  "Please provide a command"
+        usage " tldr <package>"
+        return 1
+    fi
+}
+
 # GIT --------------------------------------------------------------------------
 
 # ga: Stage files (defaults to all) and show status
@@ -64,14 +86,10 @@ gm() {
 
 # Custom Snapper manual snapshot function
 snapper-create() {
-    local RED='\033[0;31m'
-    local CYAN='\033[0;36m'
-    local GREEN='\033[0;32m'
-    local NC='\033[0m'
 
     if [[ -z "$1" ]]; then
-        echo -e "${RED}Error:${NC} Please provide a description."
-        echo -e "${CYAN}Usage:${NC} snapper-create <description>"
+        error "Please provide a description."
+        usage "snapper-create <description>"
         return 1
     fi
 
