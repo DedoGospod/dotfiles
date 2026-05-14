3673b0aace0ab1c8a791c0087612c0623130a92e
DedoGospod <dylanlazarov2002@protonmail.com>
2026-05-14 15:50:22 +0800

    refactored inputs

--- hypr/.config/hypr/lua/input.lua
+++ hypr/.config/hypr/lua/input.lua
@@ -4,23 +4,22 @@
 
 hl.config({
     input = {
+        -- Keyboard settings
         kb_layout  = "us",
         kb_variant = "",
         kb_model   = "",
         kb_options = "",
         kb_rules   = "",
 
+        -- Mouse settings
         follow_mouse = 1,
         sensitivity = 0,
         accel_profile = "flat",
         force_no_accel = true,
-
-        touchpad = {
-            natural_scroll = false,
-        },
     },
 })
 
+-- Touchpad gestures
 hl.gesture({
     fingers = 3,
     direction = "horizontal",
