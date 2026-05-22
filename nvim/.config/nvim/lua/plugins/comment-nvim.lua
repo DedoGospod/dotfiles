c5263898567c08a374bd72db845ac7cc08892982
DedoGospod <dylanlazarov2002@protonmail.com>
2026-05-22 20:56:43 +0800

    re-formatted

--- nvim/.config/nvim/lua/plugins/comment-nvim.lua
+++ nvim/.config/nvim/lua/plugins/comment-nvim.lua
@@ -1,7 +1,13 @@
 return {
-  'numToStr/Comment.nvim',
-  opts = {},
-  keys = {
-    { '<C-c>', function() require('Comment.api').toggle.linewise.current() end, desc = 'Toggle line comment' },
-  },
+	"numToStr/Comment.nvim",
+	opts = {},
+	keys = {
+		{
+			"<C-c>",
+			function()
+				require("Comment.api").toggle.linewise.current()
+			end,
+			desc = "Toggle line comment",
+		},
+	},
 }
