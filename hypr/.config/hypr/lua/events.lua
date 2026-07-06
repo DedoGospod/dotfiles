f4abc5fbe93df762f00790bed10282f864b22a09
DedoGospod <dylanlazarov2002@protonmail.com>
2026-07-06 15:19:40 +0800

    added brightness change events for gaming windows and stremio

--- hypr/.config/hypr/lua/events.lua
+++ hypr/.config/hypr/lua/events.lua
@@ -1,7 +1,8 @@
-local is_stremio_focused = false
+-- .config/hypr/lua/events.lua
 
+local is_stremio_focused = false
 hl.on("window.active", function(window)
-    if window ~= nil and window.class == "com.stremio.stremio" then
+    if window ~= nil and window.class == "com.stremio.Stremio" then
         if not is_stremio_focused then
             hl.exec_cmd("ddcutil setvcp 10 100 --async")
             is_stremio_focused = true
@@ -10,6 +11,21 @@ hl.on("window.active", function(window)
         if is_stremio_focused then
             hl.exec_cmd("ddcutil setvcp 10 50 --async")
             is_stremio_focused = false
+        end
+    end
+end)
+
+local is_game_active = false
+hl.on("window.active", function(window)
+    if window ~= nil and window.content_type == "game" then
+        if not is_game_active then
+            hl.exec_cmd("ddcutil setvcp 10 100 --async")
+            is_game_active = true
+        end
+    else
+        if is_game_active then
+            hl.exec_cmd("ddcutil setvcp 10 50 --async")
+            is_game_active = false
         end
     end
 end)
