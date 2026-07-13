899c227af19cc486de632ed69b3ac75258aac70b
DedoGospod <dylanlazarov2002@protonmail.com>
2026-07-14 01:37:41 +0800

    modularized configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,77 +1,18 @@
 { config, pkgs, ... }:
 
 {
-    imports = [
-      ./hardware-configuration.nix
+  imports = [
+    ./hardware-configuration.nix
+    ./modules/system.nix
+    ./modules/services.nix
+    ./modules/security.nix
 
-      # --- DESKTOP MODULES ---
-      ./modules/hyprland.nix
-    ];
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos";
-  time.timeZone = "Australia/Perth";
-
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
-    xwayland.enable = true;
-  };
-
-    users.users.dylan = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-  };
-
-    environment.systemPackages = with pkgs; [
-    vim
-    git
-
+    # --- DESKTOP MODULES
+    ./modules/hyprland.nix
   ];
 
-services = {
-  openssh.enable = true;
-  fstrim.enable = true;
-};
-
-  networking = {
-    networkmanager.enable = true;
-  };
-
-  security = {
-    apparmor.enable = true;
-  };
-
-  hardware = {
-    bluetooth.enable = true;
-  };
-
-  services.scx = {
-    enable = true;
-    scheduler = "scx_lavd";
-    extraArgs = [ "--autopower" ];
-};
-
-  services.ananicy = {
-    enable = true;
-    package = pkgs.ananicy-cpp;
-    rulesProvider = pkgs.ananicy-rules; 
-  };
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.caskaydia-cove
-    ubuntu-classic
-    font-awesome
-    dejavu_fonts
-    liberation_ttf
-    croscore-fonts
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-color-emoji
-  ];
-
+  # Global settings
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
   system.stateVersion = "25.05";
 }
