011b53dae7e054e8acff232a1e3be57efc4d099f
DedoGospod <dylanlazarov2002@protonmail.com>
2026-07-14 14:12:14 +0800

    moduarized

--- home/files.nix
+++ home/files.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 let
   linkDot = repoPath: {
@@ -9,30 +9,30 @@ in {
   # Dotfiles
   home.file = {
     # Directories
-    ".config/backgrounds" = linkDot "backgrounds/.config/backgrounds";
-    ".config/fastfetch"   = linkDot "fastfetch/.config/fastfetch";
-    ".config/hypr"        = linkDot "hypr/.config/hypr";
-    ".config/pypr"        = linkDot "hypr/.config/pypr";
-    ".config/kitty"       = linkDot "kitty/.config/kitty";
-    ".config/MangoHud"    = linkDot "mangohud/.config/MangoHud";
-    ".config/mpv"         = linkDot "mpv/.config/mpv";
-    ".config/nvim"        = linkDot "nvim/.config/nvim";
-    ".config/projects"    = linkDot "projects/Projects";
-    ".config/scopebuddy"  = linkDot "scopebuddy/.config/scopebuddy";
-    ".config/swaync"      = linkDot "swaync/.config/swaync";
-    ".config/tmux"        = linkDot "tmux/.config/tmux";
-    ".config/autostart"   = linkDot "uwsm/.config/autostart";
-    ".config/uwsm"        = linkDot "uwsm/.config/uwsm";
-    ".config/waybar"      = linkDot "waybar/.config/waybar";
-    ".config/wofi"        = linkDot "wofi/.config/wofi";
-    ".config/yazi"        = linkDot "yazi/.config/yazi";
-    ".config/zsh"         = linkDot "zsh/.config/zsh";
+    ".config/backgrounds"                   = linkDot "backgrounds/.config/backgrounds";
+    ".config/fastfetch"                     = linkDot "fastfetch/.config/fastfetch";
+    ".config/hypr"                          = linkDot "hypr/.config/hypr";
+    ".config/pypr"                          = linkDot "hypr/.config/pypr";
+    ".config/kitty"                         = linkDot "kitty/.config/kitty";
+    ".config/MangoHud"                      = linkDot "mangohud/.config/MangoHud";
+    ".config/mpv"                           = linkDot "mpv/.config/mpv";
+    ".config/nvim"                          = linkDot "nvim/.config/nvim";
+    ".config/projects"                      = linkDot "projects/Projects";
+    ".config/scopebuddy"                    = linkDot "scopebuddy/.config/scopebuddy";
+    ".config/swaync"                        = linkDot "swaync/.config/swaync";
+    ".config/tmux"                          = linkDot "tmux/.config/tmux";
+    ".config/autostart"                     = linkDot "uwsm/.config/autostart";
+    ".config/uwsm"                          = linkDot "uwsm/.config/uwsm";
+    ".config/waybar"                        = linkDot "waybar/.config/waybar";
+    ".config/wofi"                          = linkDot "wofi/.config/wofi";
+    ".config/yazi"                          = linkDot "yazi/.config/yazi";
+    ".config/zsh"                           = linkDot "zsh/.config/zsh";
     ".config/wayland-pipewire-idle-inhibit" = linkDot "wayland-pipewire-idle-inhibit/.config/wayland-pipewire-idle-inhibit";
 
     # Individual files
-    ".zshenv"               = linkDot "zsh/.zshenv";
-    ".config/starship.toml" = linkDot "starship/.config/starship.toml";
-    ".config/topgrade.toml" = linkDot "topgrade/.config/topgrade.toml";
-    ".config/kwalletrc"     = linkDot "kwalletrc/.config/kwalletrc";
+    ".zshenv"                               = linkDot "zsh/.zshenv";
+    ".config/starship.toml"                 = linkDot "starship/.config/starship.toml";
+    ".config/topgrade.toml"                 = linkDot "topgrade/.config/topgrade.toml";
+    ".config/kwalletrc"                     = linkDot "kwalletrc/.config/kwalletrc";
   };
 }
---
+++ home/xdg-setup.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Base XDG Directories
+  xdg = {
+    enable = true;
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
+  };
+
+  # Centralized Environment Variables
+  home.sessionVariables = {
+    # Base XDG
+    XDG_DATA_HOME   = "${config.home.homeDirectory}/.local/share";
+    XDG_CONFIG_HOME = "${config.home.homeDirectory}/.config";
+    XDG_STATE_HOME  = "${config.home.homeDirectory}/.local/state";
+    XDG_CACHE_HOME  = "${config.home.homeDirectory}/.cache";
+
+    # Application Paths
+    CARGO_HOME      = "${config.xdg.dataHome}/cargo";
+    RUSTUP_HOME     = "${config.xdg.dataHome}/rustup";
+    GNUPGHOME       = "${config.xdg.dataHome}/gnupg";
+    GOPATH          = "${config.xdg.dataHome}/go";
+    PYTHONHISTORY   = "${config.xdg.stateHome}/python/history";
+    DOTNET_CLI_HOME = "${config.xdg.dataHome}/dotnet";
+    NUGET_PACKAGES  = "${config.xdg.dataHome}/nuget/packages";
+    PARALLEL_HOME   = "${config.xdg.configHome}/parallel";
+    
+    # NPM Config
+    NPM_CONFIG_USERCONFIG = "${config.xdg.configHome}/npm/npmrc";
+    NPM_CONFIG_INIT_MODULE = "${config.xdg.configHome}/npm/config/npm-init.js";
+    NPM_CONFIG_CACHE      = "${config.xdg.cacheHome}/npm";
+    NPM_CONFIG_TMP        = "${config.xdg.runtimeDir}/npm";
+
+    # Graphics/Misc
+    GTK2_RC_FILES           = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+    CUDA_CACHE_PATH         = "${config.xdg.cacheHome}/nv";
+    __GL_SHADER_DISK_CACHE_PATH = "${config.xdg.cacheHome}/nv";
+    ANDROID_USER_HOME       = "${config.xdg.dataHome}/android";
+  };
+
+  # Path Management
+  home.sessionPath = [
+    "${config.home.homeDirectory}/.local/bin"
+    "${config.xdg.dataHome}/cargo/bin"
+    "${config.xdg.dataHome}/go/bin"
+  ];
+
+}
