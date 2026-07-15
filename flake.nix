e987522fcfaeb688e12e23d0cbc7ac02038f1cf1
DedoGospod <dylanlazarov2002@protonmail.com>
2026-07-15 15:07:57 +0800

    multi-system support added (VM + Desktop)

--- flake.nix
+++ flake.nix
@@ -2,28 +2,40 @@
   description = "NixOS";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; #nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager"; #/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
-    nixosConfigurations.nixos-hyprland = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.dylan = import ./home.nix;
-            backupFileExtension = "backup";
-          };
-        }
-      ];
+  outputs = { self, nixpkgs, home-manager, ... }: 
+  let
+    sharedModules = [
+      ./configuration.nix
+      home-manager.nixosModules.home-manager
+      {
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          users.dylan = import ./home.nix;
+          backupFileExtension = "backup";
+        };
+      }
+    ];
+  in {
+    nixosConfigurations = {
+      # Desktop Profile
+      desktop-hyprland = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = sharedModules ++ [ ./hosts/desktop.nix ];
+      };
+
+      # Virtual Machine Profile
+      vm-hyprland = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = sharedModules ++ [ ./hosts/vm.nix ];
+      };
     };
   };
 }
