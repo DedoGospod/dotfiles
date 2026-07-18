36d929485163de56d898953275ecab7a9c8e5da6
DedoGospod <dylanlazarov2002@protonmail.com>
2026-07-18 18:00:38 +0800

    added obs service

--- home/services.nix
+++ home/services.nix
@@ -1,7 +1,31 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   #services = {
     #easyeffects.enable = true;
   #};
+
+  systemd.user.services.obs = {
+    Unit = {
+      Description = "OBS Studio";
+      PartOf = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+    };
+    Service = {
+      Type = "simple";
+      ExecStartPre = "${pkgs.coreutils}/bin/rm -rf %h/.config/obs-studio/.sentinel";
+      ExecStart = "${pkgs.obs-studio}/bin/obs";
+      Restart = "on-failure";
+      RestartSec = "5s";
+      StartLimitIntervalSec = 60;
+      StartLimitBurst = 5;
+      KillSignal = "SIGKILL";
+    };
+    #Install = {
+    #    WantedBy = [ "graphical-session.target" ];
+    #};
+
+  };
+
+
 }
