c4407f638d315d825706911a561a85414ec22ccf
DedoGospod <dylanlazarov2002@protonmail.com>
2026-07-18 18:00:35 +0800

    added events to occur when a game is the active window

--- hypr/.config/hypr/lua/events.lua
+++ hypr/.config/hypr/lua/events.lua
@@ -1,30 +1,34 @@
 -- .config/hypr/lua/events.lua
 
-local is_stremio_focused = false
-hl.on("window.active", function(window)
-    if window ~= nil and window.class == "com.stremio.Stremio" then
-        if not is_stremio_focused then
-            hl.exec_cmd("ddcutil setvcp 10 100 --async")
-            is_stremio_focused = true
-        end
-    else
-        if is_stremio_focused then
-            hl.exec_cmd("ddcutil setvcp 10 50 --async")
-            is_stremio_focused = false
-        end
-    end
-end)
+-- local is_stremio_focused = false
+-- hl.on("window.active", function(window)
+    -- if window ~= nil and window.class == "com.stremio.Stremio" then
+        -- if not is_stremio_focused then
+            -- hl.exec_cmd("ddcutil setvcp 10 100 --async")
+            -- is_stremio_focused = true
+        -- end
+    -- else
+        -- if is_stremio_focused then
+            -- hl.exec_cmd("ddcutil setvcp 10 50 --async")
+            -- is_stremio_focused = false
+        -- end
+    -- end
+-- end)
 
 local is_game_active = false
 hl.on("window.active", function(window)
     if window ~= nil and window.content_type == "game" then
         if not is_game_active then
             hl.exec_cmd("ddcutil setvcp 10 100 --async")
+            hl.exec_cmd("systemctl --user stop hyprsunset.service")
+            hl.exec_cmd("swaync-client -dn")
             is_game_active = true
         end
     else
         if is_game_active then
             hl.exec_cmd("ddcutil setvcp 10 50 --async")
+            hl.exec_cmd("systemctl --user start hyprsunset.service")
+            hl.exec_cmd("swaync-client -df")
             is_game_active = false
         end
     end
